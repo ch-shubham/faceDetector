aad5571519a981a03182a74343d2534cd88e2510
ch-shubham <shubham128166@gmail.com>
2019-05-26 16:25:41 +0530

    first commit, adding files-6

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.put('/image',(req,res) => {image.handleImage(req,res,db)});
 
 app.post('/imageurl',(req,res) => {image.handleApiCall(req,res)});
 
-app.listen(process.env.PORT || 3000, () => console.log(`App is working successfully on port ${PORT`));
+app.listen(process.env.PORT || 3000, () => console.log(`App is working successfully on port ${PORT}`));
 
 
 
